6a652c31d40ba22893e7531b16c1e335f175fac4
RSAdrian9 <adrian4rs@gmail.com>
2023-12-17 18:32:50 +0100

    ChangeTab2forHomeNotes

--- src/app/tabs/tabs.routes.ts
+++ src/app/tabs/tabs.routes.ts
@@ -14,7 +14,7 @@ export const routes: Routes = [
       {
         path: 'tab2',
         loadComponent: () =>
-          import('../tab2/tab2.page').then((m) => m.Tab2Page),
+          import('../home-notes/home-notes.page').then((m) => m.HomeNotesPage),
       },
       {
         path: 'tab3',
